0728498068ab1f9545cab5d141128858acdc86a2
Isuru <icbjayasinghe@gmail.com>
2021-07-05 20:12:32 +0530

    Add initial commit

---
+++ tracking-api/src/main/java/com/alnitech/locationtrackerapi/LocationTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.alnitech.locationtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationTrackerApiApplication.class, args);
+    }
+
+}
